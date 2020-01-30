dc83e4b8a267602678832962acb84cb889f6ae59
John Doe <nadia643@gmail.com>
2020-01-30 11:53:27 +0000

    added books

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// state, this.setState = hold/access/change data of component
+// props = read only
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from "react";
+import Booklist from "./Booklist";
+const App = () => (
+  <section>
+    <h1>This is our Application</h1>
+    <Booklist />
+  </section>
+);
 
 export default App;
